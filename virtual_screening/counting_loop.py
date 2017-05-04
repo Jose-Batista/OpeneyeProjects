68c3bb5741d377ecffb0c2d2ca1d62096d7558ad
Charles-Elie Gorin <gorin@koebes.local>
2017-05-04 10:44:20 +0200

    Create a script that loops through a Molecule file to count the number of Molecules

---
+++ virtual_screening/counting_loop.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from openeye.oechem import *
+from openeye.oegraphsim import *
+
+import sys
+import os
+import random
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def count_database(database):
+    ifs = oemolistream()
+
+    if not ifs.open(database):
+        OEThrow.Fatal("Unable to open inputfile" )
+
+    count = 0
+    for mol in ifs.GetOEMols():
+        count += 1
+    return count
+
+def main(argv=[__name__]):
+    itf = OEInterface(InterfaceData, argv)
+
+    ind = itf.GetString("-in_database")
+
+    count = count_database(ind)
+    print(count)
+
+InterfaceData = """
+!PARAMETER -in_database
+    !ALIAS -id
+    !TYPE string
+    !BRIEF Input Database of Molecules
+    !REQUIRED true
+    !KEYLESS 1
+!END
+
+"""
+
+if __name__ == "__main__":
+        sys.exit(main(sys.argv))
