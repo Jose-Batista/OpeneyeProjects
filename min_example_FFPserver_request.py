c1c912ce2378ebf1d96343c6d9c14229f33b9937
Charles-Elie Gorin <gorin@turing.localdomain>
2017-05-09 17:10:50 +0200

    Introduced the fastfp request in the script

--- min_example_FFPserver_request.py
+++ min_example_FFPserver_request.py
@@ -11,23 +11,23 @@ data = response.json()
 for entry in data[ 'databases' ]:
     print( entry )
 
-print( "\nGet more information about the loaded databases" )
-for entry in data['databases']:
-    url = "%s/%s" %( baseurl, entry )
-    response = requests.get( url )
-    res = response.json()
-    print( res )
+#print( "\nGet more information about the loaded databases" )
+#for entry in data['databases']:
+#    url = "%s/%s" %( baseurl, entry )
+#    response = requests.get( url )
+#    res = response.json()
+#    print( res )
 
 print( "\nPerform a query on first db" )
-url = "%s/%s/hitlist?smiles=%s" %( baseurl, data['databases'][0],"c1ccccc1CCC1CCCCC1")
+url = "%s/%s/hitlist?smiles=%s&oformat=csv" %( baseurl, data['databases'][0],"c1ccccc1CCC1CCCCC1")
 response = requests.get( url )
-res = response.json()
-for entry in res:
-    print( "MolID: %-20s  SimVal: %.4f" %( entry, res[ entry ] ) )
+print(response.content)
+#for entry in res:
+#    print( "MolID: %-20s  SimVal: %.4f" %( entry, res[ entry ] ) )
 
-print( "\nPerform a query on first db with cutoff and maxhits" )
-url = "%s/%s/hitlist?smiles=%s&cutoff=%f&maxhits=%d" %( baseurl, data['databases'][0],"c1ccccc1CCC1CCCCC1", 0.3, 5)
-response = requests.get( url )
-res = response.json()
-for entry in res:
-    print( "MolID: %-20s  SimVal: %.4f" %( entry, res[ entry ] ) )
+#print( "\nPerform a query on first db with cutoff and maxhits" )
+#url = "%s/%s/hitlist?smiles=%s&cutoff=%f&maxhits=%d" %( baseurl, data['databases'][0],"c1ccccc1CCC1CCCCC1", 0.3, 5)
+#response = requests.get( url )
+#res = response.json()
+#for entry in res:
+#    print( "MolID: %-20s  SimVal: %.4f" %( entry, res[ entry ] ) )
