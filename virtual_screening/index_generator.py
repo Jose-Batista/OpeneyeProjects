1548c08551f14e02d59c669f861763fbe5f0a3a7
Charles-Elie Gorin <gorin@koebes.local>
2017-05-15 15:49:45 +0200

    Debug : Problem in indexes order solved

--- virtual_screening/index_generator.py
+++ virtual_screening/index_generator.py
@@ -7,42 +7,43 @@ import os
 import random
 
 def CountDatabase(database):
-	ifs = oemolistream()
+    ifs = oemolistream()
 
-	if not ifs.open(database):
-		OEThrow.Fatal("Unable to open inputfile" )
+    if not ifs.open(database):
+        OEThrow.Fatal("Unable to open inputfile" )
 
-	count = 0
+    count = 0
 
-	for mol in ifs.GetOEMols():
-		count += 1
-	return count
+    for mol in ifs.GetOEMols():
+        count += 1
+    return count
 
 def RandomIndex(total, ratio, iteration, index_out):
-	nb_index = total//(ratio + 1)
+    nb_index = total//(ratio + 1)
 
-	with open(index_out, "w"): pass
-	
-	for iter in range (iteration):
-		i = 0
+    with open(index_out, "w"): pass
+    
+    for iter in range (iteration):
+        i = 0
 
-		index_set = set()
-		while i < nb_index :
-			index = random.randint(0, total - 1)
-			if index in index_set:
-				continue
-			else : 
-				index_set.add(index)
-				i += 1
+        index_set = set()
+        while i < nb_index :
+            index = random.randint(0, total - 1)
+            if index in index_set:
+                continue
+            else : 
+                index_set.add(index)
+                i += 1
 
-		index_log = open(index_out, 'a')
-		index_log.write("set N°%d: " % iter)
-		for index in index_set:
-			index_log.write(str(index) + " ")
-		index_log.write("\n")
-		index_log.close()
+        index_log = open(index_out, 'a')
+        index_log.write("set N°%d: " % iter)
+        for index in range(total - 1):
+            if index in index_set:
+                index_log.write(str(index) + " ")
+        index_log.write("\n")
+        index_log.close()
 
-	return index_set
+    return index_set
 
 total = 2040
 nb_index = total//(3)
@@ -50,51 +51,51 @@ index_out = 'clean_databases/FA10/index_log_FA10.txt'
 iteration = 5
 
 def main(argv=[__name__]):
-	itf = OEInterface(InterfaceData, argv)
+    itf = OEInterface(InterfaceData, argv)
 
-	ina = itf.GetString("-act_database")
-	ratio = itf.GetInt("-ratio")
-	iteration = itf.GetInt("-iteration")
-	index_out = itf.GetString("-output")
+    ina = itf.GetString("-act_database")
+    ratio = itf.GetInt("-ratio")
+    iteration = itf.GetInt("-iteration")
+    index_out = itf.GetString("-output")
 
-	tot_range = CountDatabase(ina)
+    tot_range = CountDatabase(ina)
 
-	RandomIndex(tot_range, ratio, iteration, index_out)
+    RandomIndex(tot_range, ratio, iteration, index_out)
 
 InterfaceData = """
 !PARAMETER -act_database
-	!ALIAS -ad
-	!TYPE string
-	!BRIEF Database to be Counted
-	!REQUIRED true
-	!KEYLESS 1
+    !ALIAS -ad
+    !TYPE string
+    !BRIEF Database to be Counted
+    !REQUIRED true
+    !KEYLESS 1
 !END
 
 !PARAMETER -ratio
-	!ALIAS -r
-	!TYPE int
-	!BRIEF Ratio of index generated
-	!REQUIRED true
-	!KEYLESS 2
+    !ALIAS -r
+    !TYPE int
+    !BRIEF Ratio of index generated
+    !REQUIRED true
+    !KEYLESS 2
 !END
 
 !PARAMETER -iteration
-	!ALIAS -it
-	!TYPE int
-	!BRIEF Number of Iterations wanted for the Test
-	!REQUIRED true
-	!KEYLESS 3
+    !ALIAS -it
+    !TYPE int
+    !BRIEF Number of Iterations wanted for the Test
+    !REQUIRED true
+    !KEYLESS 3
 !END
 
 !PARAMETER -output
-	!ALIAS -o
-	!TYPE string
-	!BRIEF Output File
-	!REQUIRED true
-	!KEYLESS 4
+    !ALIAS -o
+    !TYPE string
+    !BRIEF Output File
+    !REQUIRED true
+    !KEYLESS 4
 !END
 
 """
 
 if __name__ == "__main__":
-		sys.exit(main(sys.argv))
+        sys.exit(main(sys.argv))
